9d650ac9d23281efff5a4584d6efa01089bb58ea
rroschin <Roman.Roschin@dataart.com>
2015-08-12 16:59:25 +0200

    dynamic graph demo class renamed to be more readable

---
+++ calculation-engine/engine-core/src/main/java/com/dataart/spreadsheetanalytics/main/DynamicExecutionGraphDemo.java
@@ -0,0 +1,29 @@
+package com.dataart.spreadsheetanalytics.main;
+
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import com.dataart.spreadsheetanalytics.api.engine.IAuditor;
+import com.dataart.spreadsheetanalytics.api.engine.IEvaluator;
+import com.dataart.spreadsheetanalytics.api.model.IExecutionGraph;
+import com.dataart.spreadsheetanalytics.engine.SpreadsheetAuditor;
+import com.dataart.spreadsheetanalytics.engine.SpreadsheetEvaluator;
+import com.dataart.spreadsheetanalytics.util.print.ExecutionGraphConsolePrinter;
+
+public class DynamicExecutionGraphDemo {
+    
+    public static void main(String[] args) throws Exception {
+        
+        final String path = "src/main/resources/excel/2.xlsx";
+        // TODO: this part  should somewhere in dataprovider and Class should be something like IWorkbook or IModel
+        final XSSFWorkbook model = new XSSFWorkbook(path);
+        
+        
+        final IEvaluator evaluator = new SpreadsheetEvaluator(model);
+        final IAuditor auditor = new SpreadsheetAuditor(evaluator);
+        
+        IExecutionGraph graph = auditor.buildStaticExecutionGraph(model);
+        
+        ExecutionGraphConsolePrinter.print(graph);
+    }
+
+}
