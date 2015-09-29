4843f35b9424e0f5279d85cfeede16fe619f8727
rroschin <roschin.roman@gmail.com>
2015-09-29 18:29:58 +0200

    new excel files are added for IT

--- calculation-engine/engine-tests/engine-it-graph/src/test/java/com/dataart/spreadsheetanalytics/test/util/GraphTestUtil.java
+++ calculation-engine/engine-tests/engine-it-graph/src/test/java/com/dataart/spreadsheetanalytics/test/util/GraphTestUtil.java
@@ -3,11 +3,14 @@ package com.dataart.spreadsheetanalytics.test.util;
 import static org.assertj.core.api.StrictAssertions.assertThat;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.LineNumberReader;
 import java.io.Writer;
+import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Scanner;
 
@@ -35,6 +38,8 @@ public class GraphTestUtil {
     final static String TEST_FILES = "com.dataart.spreadsheetanalytics.test.graph";
     
     final static String GRAPH_PATHS_FILE = STANDARD_EXCELS_DIR + "_graph_paths.lst";
+    final static String TEST_CLASS_TEMPLATE = "src/test/resources/Excel_XXX_Test.java.template";
+    final static String TEST_CLASS_FILE = "src/test/java/com/dataart/spreadsheetanalytics/test/graph/Excel_XXX_Test.java";
     
     @Test
     public void it_GraphPaths_NumbrOfTestFilesGreaterThenExcels() throws IOException {
@@ -50,16 +55,27 @@ public class GraphTestUtil {
         assertThat(tests).isGreaterThan(lines);
     }
 
-    public static void generateGraphmlFileset() throws Exception {
+    public static void generateGraphmlFileset(boolean all) throws Exception {
         System.out.println("Begin.");
+
+        String testTemplate = null;
+        try (FileInputStream fis = new FileInputStream(TEST_CLASS_TEMPLATE)) {
+            byte[] b = new byte[fis.available()];
+            fis.read(b);
+            testTemplate = new String(b);
+        }
+        
         try (Scanner sc = new Scanner(Paths.get(GRAPH_PATHS_FILE))) {
 
-            System.out.println("For each line in file [" + GRAPH_PATHS_FILE + "]");
+            System.out.println("For each line in file [" + GRAPH_PATHS_FILE + "]\n");
             while (sc.hasNext()) {
                 String[] line = sc.next().split("\\$");
 
                 String path = STANDARD_EXCELS_DIR + line[0] + ".xlsx";
                 String address = line[1];
+                String filename = STANDARD_GRAPHML_DIR + line[0] + "_" + address + ".graphml";
+                
+                if (!all && Files.exists(Paths.get(filename))) { continue; }
 
                 System.out.println("Excel file [" + path + "], address [" + address + "]");
 
@@ -68,19 +84,26 @@ public class GraphTestUtil {
                 final ICellAddress addr = new CellAddress(model.dataModelId(), A1Address.fromA1Address(address));
                 final DirectedGraph dgraph = ExecutionGraph.unwrap((ExecutionGraph) auditor.buildDynamicExecutionGraph(addr));
 
-                String filename = STANDARD_GRAPHML_DIR + line[0] + "_" + address + ".graphml";
                 Writer fw = new FileWriter(filename);
 
                 GraphMLExporter exporter = new GraphWithProperertiesMLExporter(address);
                 exporter.export(fw, dgraph);
 
                 System.out.println("GraphML file is written to [" + filename + "]");
+                
+                String testFile = testTemplate.replace("[CELL_ADDRESS]", address).replace("[FILENAME]", line[0]).replace("XXX", line[0] + "_" + address);
+                try (FileOutputStream fos = new FileOutputStream(TEST_CLASS_FILE.replace("XXX", line[0] + "_" + address))) {
+                    fos.write(testFile.getBytes());
+                }
+                System.out.println("Java Test file is written to [" + TEST_CLASS_FILE.replace("XXX", line[0] + "_" + address) + "]");
+                System.out.println();
             }
         }
-        System.out.println("End.");
+        System.out.println("\nEnd.");
     }
     
     public static void main(String[] args) throws Exception {
-        generateGraphmlFileset();
+        boolean all = args.length > 0 && args[0].equals("all");
+        generateGraphmlFileset(all);
     }
 }
