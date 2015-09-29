299759b4ea1a52e1fe9e5d5f0d6b8b329c185e22
rroschin <roschin.roman@gmail.com>
2015-09-29 15:47:21 +0200

    demo and docs changes

--- calculation-engine/engine-model/src/main/java/com/dataart/spreadsheetanalytics/api/engine/IDataModelLocation.java
+++ calculation-engine/engine-model/src/main/java/com/dataart/spreadsheetanalytics/api/engine/IDataModelLocation.java
@@ -6,8 +6,6 @@ import com.dataart.spreadsheetanalytics.engine.FileSystemDataModelLocation;
 /**
  * Simple interface to provide path to {@link IDataModel}s.
  * Path can be file system, database, network, etc.
- * Each implemetation of this interface and {@link IDataProvider#updateDataModels(IDataModelLocation)}
- * should support loading from path.
  * 
  * Known implementations: {@link FileSystemDataModelLocation}.
  */
