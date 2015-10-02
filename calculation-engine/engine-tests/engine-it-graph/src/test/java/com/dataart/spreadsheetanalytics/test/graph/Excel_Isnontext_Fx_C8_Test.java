b5dfd25c32245081fefbd770fcaa6273d47fe55e
rroschin <roschin.roman@gmail.com>
2015-10-02 12:05:11 +0200

    new excel files for IT are added

---
+++ calculation-engine/engine-tests/engine-it-graph/src/test/java/com/dataart/spreadsheetanalytics/test/graph/Excel_Isnontext_Fx_C8_Test.java
@@ -0,0 +1,27 @@
+package com.dataart.spreadsheetanalytics.test.graph;
+
+import static com.dataart.spreadsheetanalytics.test.util.GraphTestUtil.STANDARD_EXCELS_DIR;
+
+import java.io.IOException;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.dataart.spreadsheetanalytics.test.SerializedGraphTest;
+
+public class Excel_Isnontext_Fx_C8_Test extends SerializedGraphTest {
+    
+    static String address = "C8";
+    static String file = "Isnontext_Fx";
+    static String path = STANDARD_EXCELS_DIR + file + ".xlsx";
+    
+    @BeforeClass
+    public static void before() throws IOException {
+        before(path, address);        
+    }
+    
+    @Test
+    public void assert_ExcelFile_SerializedGraph() {
+        super.assert_ExcelFile_SerializedGraph(file, address);
+    }
+}
