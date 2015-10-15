3e9850c68ef17b863ee4b621db7dfb250fd67a9c
rroschin <roschin.roman@gmail.com>
2015-10-15 12:29:26 +0200

    XSSF changed to generic Workbook

--- calculation-engine/engine-data/src/main/java/com/dataart/spreadsheetanalytics/engine/util/PoiFileConverter.java
+++ calculation-engine/engine-data/src/main/java/com/dataart/spreadsheetanalytics/engine/util/PoiFileConverter.java
@@ -4,9 +4,9 @@ import static org.apache.poi.common.execgraph.ExecutionGraphBuilderUtils.cellVal
 
 import java.io.IOException;
 
-import org.apache.poi.xssf.usermodel.XSSFRow;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
 
 import com.dataart.spreadsheetanalytics.api.model.IDataSet;
 import com.dataart.spreadsheetanalytics.model.DataSet;
@@ -15,14 +15,14 @@ import com.dataart.spreadsheetanalytics.model.DsRow;
 
 public class PoiFileConverter {
 
-    public static IDataSet toDataSet(XSSFWorkbook workbook) throws IOException {
-        XSSFSheet sheet = workbook.getSheetAt(0); //TODO: this works only for single sheet documents
+    public static IDataSet toDataSet(Workbook workbook) throws IOException {
+        Sheet sheet = workbook.getSheetAt(0); //TODO: this works only for single sheet documents
         
         DataSet result = new DataSet(sheet.getSheetName());
         
         for (int i = sheet.getFirstRowNum(); i <= sheet.getLastRowNum(); i++) {
             DsRow dsRow = result.createRow(); 
-            XSSFRow row = sheet.getRow(i);
+            Row row = sheet.getRow(i);
             for (int j = row.getFirstCellNum(); j <= row.getLastCellNum(); j++) {
                 DsCell cell = dsRow.createCell();
                 cell.value(cellValueToObject(row.getCell(j)));
