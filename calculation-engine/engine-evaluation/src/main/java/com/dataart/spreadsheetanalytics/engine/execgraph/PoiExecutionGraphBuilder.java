287b825ca4788a7ed0da83d75bf5fabba6bf9e1c
mtarasov <maxim.tarasov@dataart.com>
2015-11-09 19:24:27 +0200

    fixing exception in advanced tests

--- calculation-engine/engine-evaluation/src/main/java/com/dataart/spreadsheetanalytics/engine/execgraph/PoiExecutionGraphBuilder.java
+++ calculation-engine/engine-evaluation/src/main/java/com/dataart/spreadsheetanalytics/engine/execgraph/PoiExecutionGraphBuilder.java
@@ -592,14 +592,16 @@ public class PoiExecutionGraphBuilder implements IExecutionGraphBuilder {
 
     protected void reduceDuplicates(Map<IExecutionGraphVertex, List<IExecutionGraphVertex>> verticesMap, int allowedNum) {
         Set<ExecutionGraphVertex> childrenToRemove = new HashSet<>();
-        for (Entry<IExecutionGraphVertex, List<IExecutionGraphVertex>> entry : verticesMap.entrySet()) {
+        Iterator<Entry<IExecutionGraphVertex, List<IExecutionGraphVertex>>> enterator = verticesMap.entrySet().iterator();
+        while (enterator.hasNext()) {
+            Entry<IExecutionGraphVertex, List<IExecutionGraphVertex>> entry = enterator.next();
             List<IExecutionGraphVertex> values = entry.getValue();
             if (values.size() > allowedNum) {
                 for (IExecutionGraphVertex vertex : values) {
                     reassignOutgoingEdges(entry.getKey(), vertex);
                     for (ExecutionGraphEdge edge : dgraph.incomingEdgesOf(vertex)) {
                         ExecutionGraphVertex child = (ExecutionGraphVertex) dgraph.getEdgeSource(edge);
-                        if ("VALUE".equals(child.name())) {
+                        if (Type.CONSTANT_VALUE.equals(child.type())) {
                             childrenToRemove.add(child);
                         }
                     }
@@ -639,7 +641,7 @@ public class PoiExecutionGraphBuilder implements IExecutionGraphBuilder {
             result = it.next();
             while (it.hasNext()) {
                 IExecutionGraphVertex value = it.next();
-                if (dgraph.containsVertex(value)) {
+                if (dgraph.containsVertex(value) && dgraph.containsVertex(result)) {
                     reassignOutgoingEdges(result, value);
                     dgraph.removeVertex(value);
                 } else {
