9a375a87c731f0961e546bd41ada2ad1e7b5285c
mtarasov <maxim.tarasov@dataart.com>
2015-12-18 15:02:47 +0200

    adding toJson testing

--- calculation-engine/engine-tests/engine-it/src/test/java/com/dataart/spreadsheetanalytics/engine/JsonToGraphFullFlowTest.java
+++ calculation-engine/engine-tests/engine-it/src/test/java/com/dataart/spreadsheetanalytics/engine/JsonToGraphFullFlowTest.java
@@ -3,7 +3,9 @@ package com.dataart.spreadsheetanalytics.engine;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.UUID;
 
 import org.junit.BeforeClass;
@@ -19,37 +21,38 @@ import com.dataart.spreadsheetanalytics.api.model.IExecutionGraphVertex;
 import com.dataart.spreadsheetanalytics.engine.graph.ExecutionGraphVertex;
 import com.dataart.spreadsheetanalytics.model.A1Address;
 import com.dataart.spreadsheetanalytics.model.DataModelId;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
 public class JsonToGraphFullFlowTest {
     
-    static String jsonSample;
-    static ObjectNode validJsonObject;
+    static String json;
+    static ObjectNode jsonObject;
     static IDataModelId sharedId = new DataModelId(UUID.randomUUID().toString());
 
     @BeforeClass
     public static void before() throws Exception {
         
-        validJsonObject = (ObjectNode) ((ObjectNode) ((ObjectNode) new ObjectNode(JsonNodeFactory.instance)
+        jsonObject = (ObjectNode) ((ObjectNode) ((ObjectNode) new ObjectNode(JsonNodeFactory.instance)
                 .put("name", "Sheet1")
                 .put("dataModelId", sharedId.toString())
-                .set("names", new ObjectNode(JsonNodeFactory.instance)
-                                    .put("A1", "Tax")
-                                    .put("A2", "Rev")
-                                    .put("A3", "Bon")
-                                    .put("D3", "Total")))
                 .set("table", new ObjectNode(JsonNodeFactory.instance)
                                     .put("A1", 10.8)
+                                    .put("B2", 5.0)
+                                    .put("C3", "=B2+Tax")
                                     .put("A2", 1200.0)
                                     .put("A3", 30.0)
-                                    .put("B2", 5)
-                                    .put("C3", "=B2+Tax")
                                     .put("D2", "=ISEVEN(C3)")
                                     .put("D3", "=SUM(A2,A3)")))
-                .set("result", new ObjectNode(JsonNodeFactory.instance));
+                .set("result", new ObjectNode(JsonNodeFactory.instance)))
+                .set("names", new ObjectNode(JsonNodeFactory.instance)
+                        .put("A1", "Tax")
+                        .put("A2", "Rev")
+                        .put("A3", "Bon")
+                        .put("D3", "Total"));
 
-        jsonSample = DataModelDtoConverters.mapper.writeValueAsString(validJsonObject);
+        json = DataModelDtoConverters.mapper.writeValueAsString(jsonObject);
     }
 
     @Test
@@ -62,8 +65,10 @@ public class JsonToGraphFullFlowTest {
         Double d3_expected_value = 1230.0;
 
         //when
-        IDataModel model = DataModelDtoConverters.toDataModel(jsonSample);
+        IDataModel model = DataModelDtoConverters.toDataModel(json);
         IEvaluator evaluator = new SpreadsheetEvaluator(model);
+        ObjectNode actualJsonObject = DataModelDtoConverters.toJsonObject(model);
+        actualJsonObject.set("result", new ObjectNode(JsonNodeFactory.instance));
 
         //then
         assertThat(evaluator.evaluate(A1Address.fromA1Address("A1")).getResult().get()).isEqualTo(a1_expected_value);
@@ -71,6 +76,9 @@ public class JsonToGraphFullFlowTest {
         assertThat(evaluator.evaluate(A1Address.fromA1Address("C3")).getResult().get()).isEqualTo(c3_expected_value);
         assertThat(evaluator.evaluate(A1Address.fromA1Address("D2")).getResult().get()).isEqualTo(d2_expected_value);
         assertThat(evaluator.evaluate(A1Address.fromA1Address("D3")).getResult().get()).isEqualTo(d3_expected_value);
+
+        assertThat(actualJsonObject).isEqualTo(jsonObject);
+
     }
 
     @Test
@@ -99,14 +107,14 @@ public class JsonToGraphFullFlowTest {
         edgeSourceToTarget.put("A3", "SUM");
         edgeSourceToTarget.put("SUM", "D3");
 
-        Map<String, String> aliasesToNames = new HashMap<String, String>();
-        aliasesToNames.put("Tax", "A1");
-        aliasesToNames.put("Total", "D3");
-        aliasesToNames.put("Bon", "A3");
-        aliasesToNames.put("Rev", "A2");
+        Map<String, String> aliasesToAddresses = new HashMap<String, String>();
+        aliasesToAddresses.put("Tax", "A1");
+        aliasesToAddresses.put("Total", "D3");
+        aliasesToAddresses.put("Bon", "A3");
+        aliasesToAddresses.put("Rev", "A2");
 
         //when
-        IDataModel model = DataModelDtoConverters.toDataModel(jsonSample);
+        IDataModel model = DataModelDtoConverters.toDataModel(json);
         IAuditor auditor = new SpreadsheetAuditor(new SpreadsheetEvaluator(model));
         IExecutionGraph graph = auditor.buildExecutionGraph();
 
@@ -118,7 +126,7 @@ public class JsonToGraphFullFlowTest {
             Object value = vertexNameToValue.get(vertex.name());
             assertThat(vertex.value()).isEqualTo(value);
             if (vertex.alias() != null) {
-                String expectedByAlias = aliasesToNames.get(vertex.alias());
+                String expectedByAlias = aliasesToAddresses.get(vertex.alias());
                 assertThat(vertex.name()).isEqualTo(expectedByAlias);
             }
         }
